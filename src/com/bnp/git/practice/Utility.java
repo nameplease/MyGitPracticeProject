342ed0ea817b477d0bb8d6c8fe52c2f81cd24df2
prasadaraeddy anam <anam.prasadreddy@gmail.com>
2021-12-06 19:02:21 +0530

    [First Commit STS]

---
+++ src/com/bnp/git/practice/Utility.java
@@ -0,0 +1,9 @@
+package com.bnp.git.practice;
+
+import java.util.Date;
+
+public class Utility {
+	public static Date getSysDate() {
+		return new Date();
+	}
+}
---
+++ src/com/bnp/git/test/Test.java
@@ -0,0 +1,16 @@
+package com.bnp.git.test;
+
+import java.util.Date;
+
+import com.bnp.git.practice.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Date sysdate = Utility.getSysDate();
+		
+		System.out.println("System Is sysdate >>>"+sysdate);
+
+	}
+
+}
